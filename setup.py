ae8764ccdc842d6f762f899b6d7668e5c95d53fd
prkulk3009 <130364950+prkulk3009@users.noreply.github.com>
2024-03-11 11:40:44 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pranav_basic_calculator',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A basic calculator library',
+    author='Pranav K',
+    author_email='pkulkarni0410@gmail.com',
+    license='MIT',
+    keywords='calculator math',
+    url='https://github.com/prkulk3009/pranav_basic_calculator',
+    install_requires=[],
+)
